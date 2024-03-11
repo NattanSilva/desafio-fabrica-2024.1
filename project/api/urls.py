bac3ad0756c935ffd690e7effbeff485c5f9e134
NattanSilva <natanxboy@gmail.com>
2024-03-10 22:49:09 -0300

    :arrow_up: add: template, views, admin regist from charatcer table

--- project/api/urls.py
+++ project/api/urls.py
@@ -1,11 +1,14 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
+from ..views import CharactersCreateView, CharactersListView
 from .viewsets import CharacterViewset
 
 router = DefaultRouter()
 router.register('characters', CharacterViewset)
 
 urlpatterns = [
-  path('api/',include(router.urls))
+  path('api/',include(router.urls)),
+  path('', CharactersCreateView.as_view(), name="home"),
+  path('list/', CharactersListView.as_view(), name="characters_list")
 ]
--- project/views.py
+++ project/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, ListView
 
-# Create your views here.
+from .api.viewsets import CharacterViewset
+from .models import Character
+
+
+class CharactersCreateView(CreateView):
+  model = Character
+  template_name = 'character/home.html'
+  fields = ['name']
+  success_url = reverse_lazy('characters_list')
+
+
+  def post(self, request: HttpRequest, *args: str, **kwargs: reverse_lazy) -> HttpResponse:
+    return super().post(request, *args, **kwargs)
+
+class CharactersListView(ListView):
+  model = Character
+  template_name = 'character/list.html'
+  queryset = Character.objects.all()
